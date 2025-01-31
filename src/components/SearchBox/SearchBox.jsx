8b07b0d9fe30616cb19854c9e11e8ceaf187cf18
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-31 16:37:01 +0100

    update search box

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,17 +1,32 @@
-import s from './SearchBox.module.css'
+import s from './SearchBox.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter } from '../../redux/filtersSlice';
+import { selectFilterValue } from '../../redux/selectors';
 
-const SearchBox = ({ value, onSearch }) => {
-    return (
-        <div className={s.box}>
-            <p>Find contacts by name</p>
-            <input className={s.input}
-                type="text"
-                value={value}
-                onChange={e => onSearch(e.target.value)}
-            />
-        </div>
-    )
+const SearchBox = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(selectFilterValue);
 
-}
+  const handleChange = e => {
+    dispatch(changeFilter(e.target.value.toLowerCase()));
+  };
 
-export default SearchBox
+  return (
+    <div className={s.box}>
+      <label htmlFor="search" className={s.label}>
+        Find contacts by name
+      </label>
+      <input
+        id="search"
+        name="search"
+        type="text"
+        value={filter}
+        onChange={handleChange}
+        className={s.input}
+        autoComplete="off"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
