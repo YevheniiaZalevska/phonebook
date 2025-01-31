005c98250897a218c93a137e02a52ed44cec5651
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-31 16:35:39 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,15 @@ import SearchBox from "./components/SearchBox/SearchBox"
 import ContactList from "./components/ContactList/ContactList"
 
 
-import { useEffect, useState } from 'react'
-
 const App = () => {
-
-  const [numbers, setNumbers] = useState(() => {
-    const savedContact = window.localStorage.getItem("saved-contacts");
-    if (savedContact !== null) {
-      return JSON.parse(savedContact);
-    }
-  });
-
-
-
-  const [search, setSearch] = useState('');
-
-  const addContact = (newContact) => {
-    setNumbers((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
-  };
-
-  const deleteContact = (contactId) => {
-    setNumbers((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== contactId);
-    })
-  }
-
-  const searchContacts = numbers.filter((contact) =>
-    contact.name.toLowerCase().includes(search.toLowerCase()));
-
-  useEffect(() => {
-    window.localStorage.setItem("saved-contacts", JSON.stringify(numbers));
-  }, [numbers]);
-
-
 
   return (
     <div className={s.contanier}>
       <h1 className={s.title}>Phonebook</h1>
-      <ContactForm onAdd={addContact} />
-      <SearchBox value={search} onSearch={setSearch} />
-      <ContactList numbers={searchContacts} onDelete={deleteContact} />
+      <ContactForm />
+      <h2 className={s.contacts}>Contacts</h2>
+      <SearchBox />
+      <ContactList />
     </div>
   )
 }
