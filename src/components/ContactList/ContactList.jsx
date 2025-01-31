d7f2dac8c4ff521dc713172a72ffa5a3f1624c03
YevheniiaZalevska <yevhenia05@gmail.com>
2025-01-31 16:36:28 +0100

    update contact list

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,23 @@
 import Contact from '../Contact/Contact'
 import s from './ContactList.module.css'
+import { selectFilteredContacts } from '../../redux/selectors';
+import { useSelector } from 'react-redux';
 
-const ContactList = ({ numbers, onDelete }) => {
+const ContactList = () => {
+  const contacts = useSelector(selectFilteredContacts);
 
-    return (
-        <>
-            <ul>
-                {numbers.map((number) => (
-                    <li className={s.list} key={number.id}>
-                        <Contact data={number} onDelete={onDelete} />
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+  return (
+    <ul className={s.list}>
+      {contacts.map(({ id, name, number }) => (
+        <Contact
+          key={id || Math.random()}
+          id={id}
+          name={name}
+          number={number}
+        />
+      ))}
+    </ul>
+  );
+};
 
-}
-
-export default ContactList
+export default ContactList;
