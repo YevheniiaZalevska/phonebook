2c1665016ab920f96678fd7d5b0c808ec9eedd00
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-03 16:06:51 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,30 @@
 import Contact from '../Contact/Contact'
 import s from './ContactList.module.css'
-import { selectFilteredContacts } from '../../redux/selectors';
 import { useSelector } from 'react-redux';
+import {
+  selectFilteredContacts,
+  selectIsLoading,
+  selectError,
+} from '../../redux/contactsSlice';
 
 const ContactList = () => {
   const contacts = useSelector(selectFilteredContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  if (isLoading) {
+    return <p>Loading... Please wait a little</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <ul className={s.list}>
       {contacts.map(({ id, name, number }) => (
         <Contact
-          key={id || Math.random()}
+          key={id}
           id={id}
           name={name}
           number={number}
