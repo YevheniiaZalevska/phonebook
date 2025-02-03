cf62b6d78002fcb784afbab1f61a6f57716aa9b0
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-03 16:07:36 +0100

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,6 +2,9 @@ import { createSelector } from 'reselect';
 
 export const selectFilterValue = state => state.filters.name;
 export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+export const selectDeletingIds = state => state.contacts.deletingIds;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilterValue],
