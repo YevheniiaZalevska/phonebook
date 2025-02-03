58050f69e3e295ebeb56c317d60a707eedc94587
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-03 16:06:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import s from './App.module.css'
 import ContactForm from "./components/ContactForm/ContactForm"
 import SearchBox from "./components/SearchBox/SearchBox"
 import ContactList from "./components/ContactList/ContactList"
-
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchContacts } from './redux/contactsOps'
+import { selectIsLoading, selectError } from './redux/contactsSlice'
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+  dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div className={s.contanier}>
@@ -12,7 +22,13 @@ const App = () => {
       <ContactForm />
       <h2 className={s.contacts}>Contacts</h2>
       <SearchBox />
-      <ContactList />
+      {isLoading ? (
+        <p>Loading... Please wait a little</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <ContactList />
+      )}
     </div>
   )
 }
