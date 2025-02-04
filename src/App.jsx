5097b2b818fd81b5c45b85cac0184949a7776cf4
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-04 21:35:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import Layout from './components/Layout/Layout';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
 
-import HomePage from './pages/HomePage';
-import RegistrationPage from './pages/RegistrationPage';
-import LoginPage from './pages/LoginPage';
-import ContactsPage from './pages/ContactsPage';
+import HomePage from './pages/HomePage/HomePage';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ContactsPage from './pages/ContactsPage/ContactsPage';
 
 const App = () => {
   const dispatch = useDispatch();
