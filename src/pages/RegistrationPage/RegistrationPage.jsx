4a42f121843fd520cb99d2c0109fffb13ca287f1
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-04 21:45:03 +0100

    create registration page/registration page css

---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,13 @@
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import s from './RegistrationPage.module.css';
+
+const RegistrationPage = () => {
+  return (
+    <div className={s.container}>
+      <h2>Register</h2>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default RegistrationPage;
