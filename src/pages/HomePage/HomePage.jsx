299967d50b0518600d61e02c80e8858322a11150
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-04 21:43:17 +0100

    create home page/home page css

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import s from './HomePage.module.css';
+
+const HomePage = () => {
+  return (
+    <div className={s.homePage}>
+      <h1>Welcome to the Phonebook App!</h1>
+      <p>Manage your contacts easily and securely.</p>
+    </div>
+  );
+};
+
+export default HomePage;
