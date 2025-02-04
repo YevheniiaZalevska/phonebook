79d152d55b6b42545f7ebace84ad44400063efd4
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-04 11:14:20 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
-import contactsReducer from './contactsSlice';
-import filtersReducer from './filtersSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';  
+
+import contactsReducer from './contacts/slice';
+import filtersReducer from './filters/slice';
+import authReducer from './auth/slice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts', 'filters', 'auth'], 
+};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filters: filtersReducer,
+  auth: authReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware(),
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ['persist/PERSIST'], 
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
