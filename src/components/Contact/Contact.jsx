df446c9396cd3beb66e4c1ea08d306fd57f9ac8f
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-19 19:07:03 +0100

    update contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { editContact, deleteContact } from '../../redux/contacts/operations';
-import { FaUser, FaPhoneAlt } from 'react-icons/fa';
+import { FaUser, FaPhoneAlt, FaPen } from 'react-icons/fa';
 import s from './Contact.module.css';
 
 const Contact = ({ id, name, number }) => {
@@ -15,7 +15,6 @@ const Contact = ({ id, name, number }) => {
       alert('Fields cannot be empty!');
       return;
     }
-
     dispatch(editContact({ id, updatedContact: { name: newName, number: newNumber } }));
     setIsEditing(false);
   };
@@ -24,40 +23,35 @@ const Contact = ({ id, name, number }) => {
     <li className={s.item}>
       {isEditing ? (
         <div className={s.editContainer}>
-          <input
-            className={s.input}
-            type="text"
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-          <input
-            className={s.input}
-            type="text"
-            value={newNumber}
-            onChange={(e) => setNewNumber(e.target.value)}
-          />
-          <button className={s.saveBtn} onClick={handleSave}>Save</button>
-          <button className={s.cancelBtn} onClick={() => setIsEditing(false)}>Cancel</button>
+          <input className={s.input} type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
+          <input className={s.input} type="text" value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+          <div className={s.buttons}>
+            <button className={s.saveBtn} onClick={handleSave}>Save</button>
+            <button className={s.cancelBtn} onClick={() => setIsEditing(false)}>Cancel</button>
+          </div>
         </div>
       ) : (
-        <div className={s.details}>
-          <div>
-            <span className={s.icon}>
-              <FaUser />
-            </span>
-            <span className={s.name}>{name}</span>
+        <>
+          <div className={s.details}>
+            <div className={s.info}>
+              <span className={s.icon}><FaUser /></span>
+              <span className={s.name}>{name}</span>
+            </div>
+            <div className={s.info}>
+              <span className={s.icon}><FaPhoneAlt /></span>
+              <span className={s.number}>{number}</span>
+            </div>
           </div>
-          <div>
-            <span className={s.icon}>
-              <FaPhoneAlt />
-            </span>
-            <span className={s.number}>{number}</span>
-          </div>
-          <div className={s.buttons}>
-            <button className={s.editBtn} onClick={() => setIsEditing(true)}>Edit</button>
-            <button className={s.deleteBtn} onClick={() => dispatch(deleteContact(id))}>Delete</button>
-          </div>
-        </div>
+          {/* Показываем кнопки только если НЕ редактируем */}
+          {!isEditing && (
+            <div className={s.buttons}>
+              <button className={s.editBtn} onClick={() => setIsEditing(true)}>
+                <FaPen />
+              </button>
+              <button className={s.deleteBtn} onClick={() => dispatch(deleteContact(id))}>DELETE</button>
+            </div>
+          )}
+        </>
       )}
     </li>
   );
