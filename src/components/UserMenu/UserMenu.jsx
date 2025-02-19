4d240b42a0de70079b7060bdfebf4fc5348d38d7
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-19 17:54:35 +0100

    update user menu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { FiLogOut } from 'react-icons/fi';  
 
 import { logout } from '../../redux/auth/operations';
 import { selectUser } from '../../redux/auth/selectors';
@@ -11,8 +12,12 @@ const UserMenu = () => {
 
   return (
     <div className={s.userMenu}>
-      <span>Welcome, {user.name}!</span>
-      <button onClick={() => dispatch(logout())}>Logout</button>
+      <span className={s.welcomeText}>
+        <strong>WELCOME, {user?.name?.toUpperCase()}!</strong>
+      </span>
+      <button className={s.logoutButton} onClick={() => dispatch(logout())}>
+        <FiLogOut className={s.icon} />
+      </button>
     </div>
   );
 };
