158706cb590d78e9dca467006af5794a0a8f5eba
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-19 19:07:36 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,12 @@
 import { NavLink, useLocation } from 'react-router-dom';
-import { FiHome, FiUser } from 'react-icons/fi';  
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import { FiHome, FiUser } from 'react-icons/fi'; // 📌 Используем FiUser для "Contacts"
 import s from './Navigation.module.css';
 
 const Navigation = () => {
   const location = useLocation();
+  const isLoggedIn = useSelector(selectIsLoggedIn); // ✅ Проверка на авторизацию
 
   return (
     <nav className={s.nav}>
@@ -13,14 +16,19 @@ const Navigation = () => {
       >
         <FiHome className={s.icon} />
       </NavLink>
-      <NavLink
-        to="/contacts"
-        className={`${s.link} ${location.pathname === '/contacts' ? s.active : ''}`}
-      >
-        <FiUser className={s.icon} />  
-      </NavLink>
+      
+      {/* 🔥 "Contacts" отображается ТОЛЬКО если юзер вошел */}
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          className={`${s.link} ${location.pathname === '/contacts' ? s.active : ''}`}
+        >
+          <FiUser className={s.icon} />
+        </NavLink>
+      )}
     </nav>
   );
 };
 
 export default Navigation;
+
