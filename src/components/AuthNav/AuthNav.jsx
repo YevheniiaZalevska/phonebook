2ef2d98a743612e3948a6032603b15e557f7a7fd
YevheniiaZalevska <yevhenia05@gmail.com>
2025-02-24 22:17:37 +0100

    add the active button

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,14 +1,21 @@
-import { NavLink } from 'react-router-dom';
-
+import { NavLink, useLocation } from 'react-router-dom';
 import s from './AuthNav.module.css';
 
 const AuthNav = () => {
+  const location = useLocation();
+
   return (
     <div className={s.container}>
-      <NavLink to="/register" className={s.authButton}>
+      <NavLink
+        to="/register"
+        className={`${s.authButton} ${location.pathname === '/register' ? s.activeButton : ''}`}
+      >
         Register
       </NavLink>
-      <NavLink to="/login" className={s.authButton}>
+      <NavLink
+        to="/login"
+        className={`${s.authButton} ${location.pathname === '/login' ? s.activeButton : ''}`}
+      >
         Login
       </NavLink>
     </div>
